5f33647fbabd5e84f39fcf889551c4f51ed00994
aadvik-coder <80146416+aadvik-coder@users.noreply.github.com>
2021-05-09 18:26:17 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+var menu_list_array = [
+    "Deluxe Simply Cheese Pizza",
+    "Veggie Pizza",
+    "Vegan Pizza",
+    "Butter Chicken Pizza",
+    "Panner Pizza"
+]
+function getmenu(){
+    var htmldata;
+    htmldata = "<o1 class = 'menulist'>";
+    menu_list_array.sort();
+    for(var i=0; i < menu_list_array.length; i++){
+        htmldata=htmldata + '<li>' + menu_list_array[i] + '</li>'
+    }
+    htmldata = htmldata + "</ol>"
+    document.getElementById("display_menu").innerHTML = htmldata
+}
+
+function add_item(){
+    var htmldata;
+    var item = document.getElementById("add_item").value;
+    menu_list_array.push(item);
+    menu_list_array.sort();
+    htmldata="<section class='cards'>";
+    for(var i=0; i < menu_list_array.length; i++){
+        htmldata = htmldata + '<div class="card">' + '<img src="images/pizzaImg.png">' + menu_list_array[i] + '</div>';
+
+    }
+    htmldata=htmldata+"</section>";
+    document.getElementById("display_addedmenu").innerHTML = htmldata;
+
+}
